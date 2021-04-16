d30e701da84c5d59f7e49f849328423065b24b43
Ethan <ethjones1995@gmail.com>
2021-04-16 09:45:15 -0500

    fixing bugs

--- contracts/Utils/Incentivized.sol
+++ contracts/Utils/Incentivized.sol
@@ -33,37 +33,39 @@ contract Incentivized {
   function createNewIncentiveStrategy(
     address _token,
     uint _amount,
-    address _startingEpoch,
-    address _claimsPerEpoch,
-    address _lengthInEpochs
+    uint _startingEpoch,
+    uint _claimsPerEpoch,
+    uint _lengthInEpochs
   ) public
     returns (bool) {
     IERC20(_token).transferFrom(msg.sender, address(this), _amount);
-    incentives[incentiveCounter] = Incentive(_token, _amount, _startingEpoch, _claimsPerEpoch, 0 _lengthInEpochs, false);
+    incentives[incentiveCounter] = Incentive(_token, _amount, _startingEpoch, _claimsPerEpoch, 0, _lengthInEpochs, false);
     incentiveCounter++;
     return true;
   }
 
   //try to pare down all the requires
-  function incentivize(address user, uint strategy) internal returns (bool) {
+  function incentivize(uint strategy) internal returns (bool) {
     Incentive memory strat = incentives[strategy];
     require(currentEpoch >= strat.startingEpoch, "not ready");
     require(!strat.isPaused, "incentives are paused");
-    require(currentEpoch <= strat.startingEpoch + lengthInEpochs, "too late");
+    require(currentEpoch <= strat.startingEpoch + strat.lengthInEpochs, "too late");
     require(strat.claimedThisEpoch <= strat.claimsPerEpoch, "no rewards left");
     IERC20(strat.token).transfer(msg.sender, strat.amount);
     strat.claimedThisEpoch++;
     incentives[strategy] = strat;
+    return true;
   }
 
   function pauseStrategy(uint strategy) public returns (bool) {
     require(administrators[msg.sender], "not an admin");
-    incentives[strategy].isPaused == true;
+    incentives[strategy].isPaused = true;
+    return true;
   }
 
   function approveAdministrator(address newAdmin) public returns (bool) {
     require(administrators[msg.sender], "not an admin");
-    administrators[newAdmin] == true;
+    administrators[newAdmin] = true;
     return true;
   }
 }
